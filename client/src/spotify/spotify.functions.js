2ec80945fa134de09bff5d4c8ffecea15cf0ae5c
Seyi Oluwaleimu <s.oluwaleimu@gmail.com>
2020-07-29 11:30:18 -0400

    now able to extract code from url

--- client/src/spotify/spotify.functions.js
+++ client/src/spotify/spotify.functions.js
@@ -7,10 +7,24 @@ export const promptLogin = () => {
             window.location.replace(data.url);
         })
         //now to extract the code and state from url
-        .then(//seperate ''code' and 'state' value from url
+        .then(
+            //separate ''code' and 'state' value from url
+            getParam()
         );
     } catch(error) {
         console.log(`error: ${error}`)
     }
 }
 
+let getParam = () => {
+    const params = {};
+    let currentParam, temp;
+    const test = /[^?=]+/g;
+    const search = window.location.search;
+    for(let i = 0; currentParam = test.exec(search); i++){
+       i % 2 
+       ? params[temp] = currentParam[0]
+       : temp = currentParam[0];
+    }
+    return params;
+}
